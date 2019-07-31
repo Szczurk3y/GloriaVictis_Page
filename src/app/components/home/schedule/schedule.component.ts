5d6eb1efafb7e9a613edeb8b86adf5b1c3899d5a
maciek stoiński <maciek.stoinski2@gmail.com>
2019-07-31 15:54:35 +0200

    add schedule overlap

--- src/app/components/home/schedule/schedule.component.ts
+++ src/app/components/home/schedule/schedule.component.ts
@@ -7,6 +7,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ScheduleComponent implements OnInit {
 
+  public schedules = [
+    {
+      name: "schedule1",
+      path: "../../../../assets/home/schedule/schedule1.png",
+      id: '1'
+    },
+    {
+      name: "schedule2",
+      path: "../../../../assets/home/schedule/schedule1.png",
+      id: '2'
+    },
+    {
+      name: "schedule3",
+      path: "../../../../assets/home/schedule/schedule1.png",
+      id: '3'
+    },
+    {
+      name: "schedule4",
+      path: "../../../../assets/home/schedule/schedule1.png",
+      id: '4'
+    }
+  ];
+
   constructor() { }
 
   ngOnInit() {
